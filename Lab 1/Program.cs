ac4b1694a64b7a17786e8780ba7434293c19cb09
Triplix1 <vladikwww3vladik@gmail.com>
2022-09-29 12:40:55 +0300

    First Lab

---
+++ Lab 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var workWithFile = new WorkWithFile();
+
+            //workWithFile.Generate();
+
+            //var sort = new MySort("a.data");
+
+            //sort.Sort();
+
+            workWithFile.Output("B.data", 1000);
+            Console.WriteLine();
+            workWithFile.Output(34359738368 - 1024, "B.data");
+        }		
+	}
+
+}
